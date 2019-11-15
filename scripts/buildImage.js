2441772e73916366a2318e9106bbc4053d889a7b
Yudai Udagawa <nimius.rd@gmail.com>
2019-11-15 22:14:01 +0900

    Update build script

---
+++ scripts/buildImage.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const fsPromises = fs.promises
+const {COPYFILE_EXCL} = fs.constants
+const path = require('path')
+const languages = require('../website/languages').filter(locale => locale.enabled && locale.tag !== 'en')
+const {projectName} = require('../website/siteConfig')
+const distDir = path.join(__dirname, 'website', 'build', projectName, 'img')
+const srcDir = path.join(distDir, 'en')
+
+;(async () => {
+  const srcFiles = await fsPromises.readdir(srcDir)
+
+  languages.forEach(async language => {
+    const localeDir = path.join(distDir, language.tag)
+    
+    await fsPromises.mkdir(localeDir)
+      .catch(reason => console.log(reason.message))
+    await fsPromises.access(localeDir) 
+      .catch(reason => console.log(reason.message))
+    srcFiles.forEach(file => {
+      fsPromises.copyFile(
+        path.join(srcDir, file),
+        path.join(localeDir, file),
+        COPYFILE_EXCL
+      ).catch(reason => console.log(reason.message))
+    })
+  })
+})()
